6856da2346bad5375d163d28abe7231a56604c44
robert <yusheng.chen@sunplus.com>
2018-03-09 15:11:42 +0800

    [BOOT] Remove old code [Mantis   ID] N/A [Bug    Desc] N/A [Modify Desc] As title [Test   Case] N/A [Compile] OK [Coverity] N/A [Selftest] N/A [GPL-licensed & Third-party Source] N/A

--- editconfig.sh
+++
@@ -1,5 +0,0 @@
-if [ "$1" != 1 ];then
-	vim include/config.h
-else
-	vim Makefile.in
-fi
--- include/config.h
+++
@@ -1,8 +0,0 @@
-
-#define REG_BASE                0x9c000000
-#define RGST_BASE               REG_BASE
-
-#define RF_GRP(_grp, _reg) ((((_grp) * 32 + (_reg)) * 4) + REG_BASE)
-
-//FIXME
-//#define PLATFORM_8388  // enable to build 8388-compatible version
